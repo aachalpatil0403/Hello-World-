d97ae32c01b4957137533d915fc302e357f3297c
aachalpatil0403 <aachalpatil0403@gmail.com>
2025-06-23 10:25:52 +0530

    Update HelloWorld.sol

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract HelloWorld {
+    string private message;
+
+    constructor() {
+        message = "Hello, Blockchain!";
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    function greet() public pure returns (string memory) {
+        return "Hello, welcome to the Blockchain!";
+    }
+}
 
